ef13a213e369678af15a2a14bc967c0dc6832bcc
moo <yeeeeethisispinmoo7@gmail.com>
2022-03-11 11:03:16 +0800

    jack commit

--- src/main/java/com/pm/Scoring.java
+++ src/main/java/com/pm/Scoring.java
@@ -4,7 +4,8 @@ public class Scoring {
     public static void main(String[] args) {
         Student jack = new Student();
         jack.name = "Jack";
-        jack.english = 90;
+        jack.english = 20;
         jack.math = 87;
+        jack.print();
     }
 }
--- src/main/java/com/pm/Student.java
+++ src/main/java/com/pm/Student.java
@@ -4,4 +4,12 @@ public class Student {
         String name;
         int english;
         int math;
+    public void print(){
+        int average = (english+math)/2;
+        System.out.print(name + "\t" + english + "\t" + math + "\t"
+                + average );
+        if (average < 60){
+            System.out.println("*");
+        }
     }
+}
