40e5b3156d87e92163a4d934defdd6de80269d4d
moo <yeeeeethisispinmoo7@gmail.com>
2022-03-11 11:18:56 +0800

    jack commit

--- src/main/java/com/pm/Student.java
+++ src/main/java/com/pm/Student.java
@@ -4,12 +4,14 @@ public class Student {
         String name;
         int english;
         int math;
+
     public void print(){
         int average = (english+math)/2;
         System.out.print(name + "\t" + english + "\t" + math + "\t"
                 + average );
         if (average < 60){
-            System.out.println("*");
+            System.out.print("*");
         }
+        System.out.println();
     }
 }
