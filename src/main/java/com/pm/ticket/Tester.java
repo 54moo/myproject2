ac70abed927d5825fa13b427abbd0463e40d84b9
moo <yeeeeethisispinmoo7@gmail.com>
2022-03-25 12:03:39 +0800

    Ticket 2.0

---
+++ src/main/java/com/pm/ticket/Tester.java
@@ -0,0 +1,9 @@
+package com.pm.ticket;
+
+
+public class Tester {
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket(Station.TAIPEI_CITY, Station.KAOHSIUNG_CITY);
+    }
+
+}
