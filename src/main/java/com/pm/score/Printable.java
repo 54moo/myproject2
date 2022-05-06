8ddfc5b200537181fb8a538e52482d78c15fe265
moo <yeeeeethisispinmoo7@gmail.com>
2022-05-06 11:27:21 +0800

    interface

---
+++ src/main/java/com/pm/score/Printable.java
@@ -0,0 +1,5 @@
+package com.pm.score;
+
+public interface Printable {
+    public void print();
+}
--- src/main/java/com/pm/score/Student.java
+++ src/main/java/com/pm/score/Student.java
@@ -1,6 +1,6 @@
 package com.pm.score;
 
-public class Student {
+public class Student implements Printable{
     String name;
     int english;
     int math;
@@ -23,6 +23,8 @@ public class Student {
         return (english+math)/2;
     }
 
+    //覆寫 Printable
+    @Override
     public void print(){
         System.out.print(name + "\t" + english + "\t" + math + "\t"
                 + getAverage() );
@@ -31,4 +33,14 @@ public class Student {
         }
         System.out.println();
     }
+
+ /* public void print(){
+        System.out.print(name + "\t" + english + "\t" + math + "\t"
+                + getAverage() );
+        if (getAverage() < 60){
+            System.out.print("*");
+        }
+        System.out.println();
+    } */
+
 }
